02d58a0cae1e4a0fb191beb9b8cc7487a8a07948
Rahul <rahulhero93111@gmail.com>
2024-04-16 14:49:17 +0530

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+
+const app = new Hono()
+
+app.post('/api/v1/signup', (c) => {
+  const prisma = new PrismaClient({datasourceUrl: env.DATABASE_URL}).$extends(withAccelerate())
+  return c.text('signup route')
+})
+app.post('/api/v1/signin', (c) => {
+  return c.text('signin route')
+})
+app.post('/api/v1/blog', (c) => {
+  return c.text('signin route')
+})
+app.put('/api/v1/blog', (c) => {
+  return c.text('signin route')
+})
+app.get('/api/v1/blog/:id', (c) => {
+  const id = c.req.param('id')
+  console.log(id);
+  return c.text('get blog route')
+})
+// app.get('/api/v1/blog/bulk', (c) => {
+//   return c.text('get all blog')
+// })
+
+export default app
