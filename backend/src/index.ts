cb8f002f425b55d8bfceb0e7dac883a642e9c380
Rahul <rahulhero93111@gmail.com>
2024-04-16 15:30:54 +0530

    JWT sign

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,35 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-
+import { verify, decode, sign } from 'hono/jwt'
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string
+    JWT_SECRET: string
   }
 }>()
  
-app.post('/api/v1/signup', (c) => {
+app.post('/api/v1/signup',async (c) => {
   const prisma = new PrismaClient({datasourceUrl: c.env.DATABASE_URL}).$extends(withAccelerate())
-  return c.text('signup route')
+  const body = await c.req.json();
+  try {
+
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    })
+
+    const token = await sign({id: user.id}, c.env.JWT_SECRET);
+    return c.json({token});
+  }catch (error) {
+    return c.json({error: "error while signing up"})
+  }
 })
+
+
 app.post('/api/v1/signin', (c) => {
   return c.text('signin route')
 })
