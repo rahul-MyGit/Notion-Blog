e4c08218ec996b17aeff9e1ec968a9902e440bb1
Rahul <rahulhero93111@gmail.com>
2024-04-16 15:07:39 +0530

    Fixed environment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,14 @@ import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 
-const app = new Hono()
-
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+  }
+}>()
+ 
 app.post('/api/v1/signup', (c) => {
-  const prisma = new PrismaClient({datasourceUrl: env.DATABASE_URL}).$extends(withAccelerate())
+  const prisma = new PrismaClient({datasourceUrl: c.env.DATABASE_URL}).$extends(withAccelerate())
   return c.text('signup route')
 })
 app.post('/api/v1/signin', (c) => {
